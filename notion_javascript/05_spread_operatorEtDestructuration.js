13d5e207866146fccde84e4b2d4cb29ef5dd2158
Malik Ibo <malik97un@gmail.com>
2025-10-07 14:07:58 +0200

    Initialize React project with Vite, add essential files and configurations
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added Vite logo SVG to public directory.
    - Created App.css for styling the application layout and components.
    - Implemented App.jsx as the main application component with state management and basic UI.
    - Included React logo SVG in assets directory.
    - Established global styles in index.css for consistent theming and layout.
    - Set up main.jsx for rendering the application with StrictMode.
    - Configured Vite with vite.config.js to use React plugin.

--- notion_javascript/05_spread_operatorEtDestructuration.js
+++ notion_javascript/05_spread_operatorEtDestructuration.js
@@ -1,97 +1,140 @@
-const numbresOne = [1, 2, 3, 4, 5];
+// 1. Destructuration sur les tableaux
+
+const numbersOne = [1, 2, 3, 4, 5];
 const numbersTwo = [6, 7, 8, 9, 10];
-const objOne = {one:1,two:2,three:3}
 
-console.log(...numbresOne);
+console.log(...numbersOne);
+console.log(1, 2, 3, 4, 5); // Equivalent
 
-const copy1 = [...numbresOne]
+// Pour copie propre d'un tableau
+const copy1 = [...numbersOne];
 console.log(copy1);
-const copyobj = {...objOne}
-console.log(copyobj);
-
 
 copy1.push(6);
-console.log(numbresOne, copy1);
+console.log(numbersOne, copy1);
 
-// Pour concaténer (/fusionner)deux tableaux
-const allNumbers = [...numbresOne, ...numbersTwo]
+// Pour concaténer (/fusionner) deux tableaux
+const allNumbers = [...numbersOne, ...numbersTwo];
 console.log(allNumbers);
 
-// En parametre d'une function
+//2. Sprad operator En paramètres d'une fonction
 const add = (...numbers) => {
-  let result=0
-  
-  numbers.forEach((n)=> result = result + n)
-  console.log(numbers);
-  return numbers
-}
+  console.log(numbers); // Numbers est un tableaus
+  let result = 0;
+  // numbers.forEach((n) => result = result + n);
+  numbers.forEach((n) => (result += n));
 
-const addWithReduce =(...numbers) => {
-  return numbers.reduce((total, n) => total + n)
-}
+  return result;
+};
 
-const result = add(1,2,3,4,5,6,7,8,1001)
-const result2 = addWithReduce(1,2,3,4,5)
+const addWithReduce = (...numbers) => {
+  return numbers.reduce((total, n) => total + n);
+};
 
+const result = add(1, 2, 3, 4, 5, 8);
+const result2 = addWithReduce(1, 2, 3, 4, 5, 8);
+
+console.log("fonction add: ", result, result2);
+
+// 3. Destructuration sur les objets
 user = {
-  prenom: "malik",
-  nom: "ibo",
-  age: 24
-}
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+};
 
-userCopy ={...user}
+// Copie propre d'un objet
+userCopy = { ...user };
 console.log(userCopy);
 
-userCopy.prenom = "Orlane"
+userCopy.firstName = "James";
 console.log(userCopy);
 
-user2 = { ...user, city: "New York"}
+// Copie + modification/ajout de propriétés
+user2 = { ...user, city: "New York" }; // rajoute un propriété
 console.log(user2);
 
+user2 = { ...user, age: 30 }; // copier en modifiant l'age
+console.log(user2);
 
+// Concaténation de 2 objets
 userInfo = {
-  num: "01234567",
-  city: "Ville Malik"
-}
+  numberPhone: "0123456789",
+  city: "Paris",
+};
 
-user3 = {
-  ...user, userInfo
-}
-
+user3 = { ...user, ...userInfo };
 console.log(user3);
 
+// 4. Destruction
 const cars = ["Mustang", "Corvette", "F-150", "Focus"];
 
-const [car1, car2, car3, car4] = cars
+const [car1, car2, car3, car4] = cars; // Equivalent aux lignes ci-dessous
+// const car1 = cars[0];
+// const car2 = cars[1];
+// const car3 = cars[2];
+// const car4 = cars[3];
 
 console.log(car1, car2, car3, car4);
+
+const [car5, , , car6] = cars;
+console.log(car5, car6);
 
 const calculate = (a, b) => {
   const add = () => a + b;
   const sub = () => a - b;
   const mul = () => a * b;
   const div = () => a / b;
- 
+
   return [add, sub, mul, div];
 };
 
-const [add2, sub, , div] = calculate(2 , 1)
-console.log(add(), sub(), div());
+const [calAdd, calSub, , calDiv] = calculate(2, 1);
+console.log(calAdd(), calSub(), calDiv());
 
-const numbers = [1,2,3,4,5,6,7,8]
-const [a, b, ...c] = numbers
+// 5. Destructuration + Spread operator
+const numbers = [1, 2, 3, 4, 5];
+const [a, b, ...c] = numbers;
+// const [a, b, ...rest] = numbers;  // On retrouve souvent '...rest' , mais on peut choisir le nom que l'on souhaite
 
 console.log(c);
 
+// 6. Destructuration sur les objets
 const person = {
   firstName: "John",
   lastName: "Doe",
   age: 25,
 };
 
-const {d,e, ...f} = person
-console.log(f);
+const { age, lastName, firstName } = person;
+console.log(age, lastName, firstName);
 
+// Choisir un nom de variable différent de celui de l'attribut de l'objet
+const { age: personAge } = person;
+
+const calculateObj = (a, b) => {
+  const add = () => a + b;
+  const sub = () => a - b;
+  const mul = () => a * b;
+  const div = () => a / b;
+
+  //   return { add: add, sub: sub, mul: mul, div: div };
+  return { add, sub, mul, div }; // équivalent à la ligne ci-dessus
+};
+
+// L'objet retourné par la fonction calculateObj :
+{
+  add: () => a + b;
+  sub: () => a - b;
+  mul: () => a * b;
+  div: () => a / b;
+}
+
+const { div } = calculateObj(2, 1); // importer / récupérer la fonction
+
+const resultDiv = div();
+
+console.log(div());
 
 
 
--- notion_javascript/06_exercice.js
+++ notion_javascript/06_exercice.js
@@ -7,17 +7,52 @@ const taskList = [
  
 // 1. Utilisez la destructuration pour extraire le titre et le statut (completed) de la première tâche
 // Stockez-les dans des variables nommées "firstTaskTitle" et "firstTaskStatus"
-const {title} = firstTaskTitle
-const {completed} = firstTaskStatus
-console.log(firstTaskTitle);
-console.log(firstTaskStatus);
+// const tasks = [...taskList]
+// console.log(tasks);
+// const [task1, task2, task3] = tasks;
+
+// console.log(task1, task2, task3)
+
+// const [task4, ,] = tasks
+// console.log(task4);
+// const { id, title, completed } = task4;
+// let firstTaskTitle = title
+// console.log(firstTaskTitle);
+// let firstTaskStatus = completed
+// console.log(firstTaskStatus);
+
+const [{firstTaskTitle: title, firstTaskStatus:completed}] = taskList
+
+
+
 
  
 // 2. Utilisez le spread operator pour créer une copie du tableau taskList
 // nommée "taskListCopy"
+
+const taskListCopy = [taskList]
+console.log(taskListCopy);
+
  
 // 3. Copier le dernier objet de la liste, remplacer le title par 'Faire de la musique'
 // + ajouter une nouvelle propriété "priority" avec une valeur de 1
+
+const [,,lastTask] = taskList
+const copy = {...lastTask, title: "Faire de la musique", priority: 1}
+console.log(copy);
+
+
+// 4. faire une fonction aui affiche les éléments passés en parametre dans un console.log
+// --> On doit pourvoir passer autant de parametres que l'on veut quand appelle la fonction
+
+const calculate = (...param) => {
+  param.forEach((p) => console.log(p)
+  )
+}
+calculate(5,1)
  
-// 4. Faire un copie du tableau en ne gardant que les objets 'completed' qui sont égales à 'true'
+// 5. Faire un copie du tableau en ne gardant que les objets 'completed' qui sont égales à 'true'
 //  --> Vous pouvez le faire aec le .filter()
+
+const taskFiltered = taskList.filter((task) => task.completed === true) // pas obligé de mettre true parce que c'est déjà le cas
+console.log(taskFiltered);
